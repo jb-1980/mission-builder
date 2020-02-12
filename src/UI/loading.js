1931c0b406cb96ff8da106722cf849de9635e3d8
Joseph Gilgen <gilgenlabs@gmail.com>
2020-02-12 08:04:06 -0700

    improved loading

--- src/UI/loading.js
+++ src/UI/loading.js
@@ -1,18 +1,32 @@
 import React from "react"
 import { AtomSpinner } from "./atom"
 
-export const Loading = ({ message, style }) => (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      width: "100%",
-      ...style,
-    }}
-  >
-    <h3>{message}</h3>
-    <AtomSpinner />
-  </div>
-)
+export const Loading = ({ message, style }) => {
+  const [paint, setPaint] = React.useState(false)
+
+  React.useEffect(() => {
+    let timeout
+    if (!paint) {
+      timeout = setTimeout(setPaint, 500, true)
+    }
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [paint])
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        ...style,
+      }}
+    >
+      <h3>{message}</h3>
+      <AtomSpinner />
+    </div>
+  )
+}
